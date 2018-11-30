b7424714a179ca1382badc19a9a83a5c3ce58b1c
Kudzanayi Dzvairo <kudzanayi@Kudzanayis-MacBook-Air.local>
2018-11-29 23:05:40 -0500

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,24 +1,26 @@
-def roll_call_dwarves(array)
-   array.each_with_index do |name, index|
-     puts "#{index+1}. #{name}"
+def roll_call_dwarves(name)
+  name.each_with_index do |name, index|
+    puts "#{index+1}. #{name}"
   end
 end
 
-def summon_captain_planet(array)
-   array.collect do |word|
-     word.capitalize + "!"
-   end
+def summon_captain_planet(planeteer_calls)
+ planeteer_calls.collect do |call|
+   call.capitalize + '!'
+ end
 end
 
 def long_planeteer_calls(array)
- array.any? do |word|
-   word.length > 4
-   end
+  array.any? do |word|
+    word.length > 4
+  end
 end
 
 def find_the_cheese(array)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-    array.find do |cheese|
-      cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
-      end
-    end
+
+cheese = %w(cheddar gouda camembert)
+
+  array.find do |cheese|
+    cheese == "cheddar" || cheese == "gouda" || cheese == "camebert"
+  end
+end
